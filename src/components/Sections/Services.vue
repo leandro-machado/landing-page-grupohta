224a39ecb168a3d4e96fc9d8aa4de6eafbcbdb98
Leandro Machado <lemachadosantos18@gmail.com>
2020-08-08 00:24:53 -0300

    changes icons

--- src/components/Sections/Services.vue
+++ src/components/Sections/Services.vue
@@ -12,13 +12,13 @@
                 <div class="row margin-t-30">
                     <div class="col-lg-4 margin-t-20">
                         <div class="services-box text-center hover-effect">
-                            <i class="pe-7s-shopbag text-custom"></i>
+                            <i class="pe-7s-phone text-custom"></i>
                             <h4 class="padding-t-15">Acessorios para celulares</h4>
                         </div>
                     </div>
                     <div class="col-lg-4 margin-t-20">
                         <div class="services-box text-center hover-effect">
-                            <i class="pe-7s-shopbag text-custom"></i>
+                            <i class="pe-7s-diamond text-custom"></i>
                             <h4 class="padding-t-15">Bijuterias</h4>
                         </div>
                     </div>
@@ -32,19 +32,19 @@
                 <div class="row margin-t-30">
                     <div class="col-lg-4 margin-t-20">
                         <div class="services-box text-center hover-effect">
-                            <i class="pe-7s-shopbag text-custom"></i>
+                            <i class="pe-7s-home text-custom"></i>
                             <h4 class="padding-t-15">Decoração</h4>
                         </div>
                     </div>
                     <div class="col-lg-4 margin-t-20">
                         <div class="services-box text-center hover-effect">
-                            <i class="pe-7s-shopbag text-custom"></i>
+                            <i class="pe-7s-print text-custom"></i>
                             <h4 class="padding-t-15">Eletrônicos</h4>
                         </div>
                     </div>
                     <div class="col-lg-4 margin-t-20">
                         <div class="services-box text-center hover-effect">
-                            <i class="pe-7s-shopbag text-custom"></i>
+                            <i class="pe-7s-video text-custom"></i>
                             <h4 class="padding-t-15">Eletroportáteis</h4>
                         </div>
                     </div>
@@ -52,19 +52,19 @@
                 <div class="row margin-t-30">
                     <div class="col-lg-4 margin-t-20">
                         <div class="services-box text-center hover-effect">
-                            <i class="pe-7s-shopbag text-custom"></i>
+                            <i class="pe-7s-pendrive text-custom"></i>
                             <h4 class="padding-t-15">Informática</h4>
                         </div>
                     </div>
                     <div class="col-lg-4 margin-t-20">
                         <div class="services-box text-center hover-effect">
-                            <i class="pe-7s-shopbag text-custom"></i>
+                            <i class="pe-7s-light text-custom"></i>
                             <h4 class="padding-t-15">LED</h4>
                         </div>
                     </div>
                     <div class="col-lg-4 margin-t-20">
                         <div class="services-box text-center hover-effect">
-                            <i class="pe-7s-shopbag text-custom"></i>
+                            <i class="pe-7s-user-female text-custom"></i>
                             <h4 class="padding-t-15">Lingerie</h4>
                         </div>
                     </div>
@@ -72,19 +72,19 @@
                 <div class="row margin-t-30">
                     <div class="col-lg-4 margin-t-20">
                         <div class="services-box text-center hover-effect">
-                            <i class="pe-7s-shopbag text-custom"></i>
+                            <i class="pe-7s-portfolio text-custom"></i>
                             <h4 class="padding-t-15">Mala de viagem</h4>
                         </div>
                     </div>
                     <div class="col-lg-4 margin-t-20">
                         <div class="services-box text-center hover-effect">
-                            <i class="pe-7s-shopbag text-custom"></i>
+                            <i class="pe-7s-female text-custom"></i>
                             <h4 class="padding-t-15">Maleta de maquiagem</h4>
                         </div>
                     </div>
                     <div class="col-lg-4 margin-t-20">
                         <div class="services-box text-center hover-effect">
-                            <i class="pe-7s-shopbag text-custom"></i>
+                            <i class="pe-7s-magic-wand text-custom"></i>
                             <h4 class="padding-t-15">Maquiagens</h4>
                         </div>
                     </div>
@@ -92,19 +92,19 @@
                 <div class="row margin-t-30">
                     <div class="col-lg-4 margin-t-20">
                         <div class="services-box text-center hover-effect">
-                            <i class="pe-7s-shopbag text-custom"></i>
+                            <i class="pe-7s-cart text-custom"></i>
                             <h4 class="padding-t-15">Perfumaria</h4>
                         </div>
                     </div>
                     <div class="col-lg-4 margin-t-20">
                         <div class="services-box text-center hover-effect">
-                            <i class="pe-7s-shopbag text-custom"></i>
+                            <i class="pe-7s-wristwatch text-custom"></i>
                             <h4 class="padding-t-15">Relógio</h4>
                         </div>
                     </div>
                     <div class="col-lg-4 margin-t-20">
                         <div class="services-box text-center hover-effect">
-                            <i class="pe-7s-shopbag text-custom"></i>
+                            <i class="pe-7s-users text-custom"></i>
                             <h4 class="padding-t-15">Roupas</h4>
                         </div>
                     </div>
